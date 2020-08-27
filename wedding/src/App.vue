2217f14dc6201c7700b1587d1f1adffbebeb432a
saint37 <saint_37@163.com>
2020-08-27 17:15:49 +0800

    init

---
+++ wedding/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Wedding msg="Stupid Bread"/>
+  </div>
+</template>
+
+<script>
+import Wedding from './components/Wedding.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Wedding
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
